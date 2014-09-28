96ec10f9ec4584ccd927da037a1de64185821a22
Tilman Blumenbach <tilman+git@ax86.net>
2014-09-28 16:39:34 +0200

    Detect obsolete APEv2 tags.

--- find-replaygain.py
+++ find-replaygain.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 from collections import namedtuple
 import mutagen
+from mutagen.apev2 import APEv2, APENoHeaderError
 from mutagen.id3 import ID3, TXXX
 from mutagen.mp4 import MP4Tags
 from mutagen.oggvorbis import OggVorbis
@@ -18,6 +19,12 @@ def setup_arg_parser():
         )
 
     # Options
+    arg_parser.add_argument(
+            "-a",
+            "--ape-warning",
+            help="Print a warning if a file has ReplayGain information in obsolete APEv2 tags.",
+            action="store_true"
+        )
     arg_parser.add_argument(
             "-e",
             "--empty-only",
@@ -67,6 +74,18 @@ def get_gains(mediafile):
     return GainTuple(track_gain, album_gain)
 
 
+def has_ape_gains(filename):
+    try:
+        ape_tags = APEv2()
+        ape_tags.load(filename)
+    except APENoHeaderError:
+        return False
+
+    return \
+        "REPLAYGAIN_TRACK_GAIN" in ape_tags or \
+        "REPLAYGAIN_ALBUM_GAIN" in ape_tags
+
+
 
 args = setup_arg_parser().parse_args()
 for mediapath in args.file:
@@ -87,10 +106,21 @@ for mediapath in args.file:
         continue
 
     msg = ""
-    if not gains.track:
+    if gains.track is None:
         msg += " No TRACK gain."
-    if not gains.album:
+    if gains.album is None:
         msg += " No ALBUM gain."
 
-    if len(msg) and (not args.empty_only or not gains.track and not gains.album):
+    ape_gains_found = args.ape_warning and has_ape_gains(mediapath)
+    if ape_gains_found:
+        msg += " Has obsolete APEv2 ReplayGain tag(s)."
+
+    # Do we have something to display?
+    if not len(msg):
+        continue
+
+    # If the user only wants to get a list of files which have no ReplayGain tags at all,
+    # then also check if all ReplayGain gain tags are missing and only say something if
+    # that is the case.
+    if ape_gains_found or not args.empty_only or gains.track is None and gains.album is None:
         print "%s:%s" % (mediapath, msg)
